64beb5ec68d8020315e98a7f4ce16a8138641337
Ange Ellis <46515966+AngellisD@users.noreply.github.com>
2019-07-21 05:43:00 -0700

    Add files via upload

---
+++ static/js/app.js
@@ -0,0 +1,43 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+function BuildTable(dict) {
+    var table = d3.select("#ufo-table");
+    var tbody = table.select("tbody");
+    var trow;
+    for (var i = 0; i < dict.length; i++) {
+        trow = tbody.append("tr");
+        trow.append("td").text(dict[i]['datetime']);
+        trow.append("td").text(dict[i]['city']);
+        trow.append("td").text(dict[i]['state']);
+        trow.append("td").text(dict[i]['country']);
+        trow.append("td").text(dict[i]['shape']);
+        trow.append("td").text(dict[i]['durationMinutes']);
+        trow.append("td").text(dict[i]['comments']);
+    }
+}
+BuildTable(tableData);
+
+var submit = d3.select("#filter-btn");
+submit.on("click", function() {
+
+    d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+
+    var filteredData = table.Data.filter(sighting => sighting.datetime === inputValue);
+
+    var elmtTable = document.getElementById('table-body');
+    var tableRows = elmtTable.getElementsByTagName('tr');
+    var rowCount = tableRows.length;
+    
+    for (var x=rowCount-1; x>0; x--) {
+        elmtTable.removeChild(tableRows[x]);
+    }
+    
+    BuildTable(filteredData);
+
+});
